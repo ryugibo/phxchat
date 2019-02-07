2555318a85948f7b97733b676c9768b35adf3dc3
ryugibo <ryugibo@daum.net>
2019-02-08 00:03:30 +0900

    fix type

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :chat, ChatWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "phxchat-rgb.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :chat, Chat.Repo,
