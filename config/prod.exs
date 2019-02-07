fad1bb8c273579386a53cd993bc6ba0702c0f50d
ryugibo <ryugibo@daum.net>
2019-02-08 00:01:07 +0900

    setting for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,16 @@ use Mix.Config
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "phxchat-rgb.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :chat, Chat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +76,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
